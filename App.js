eeec5d64a6454e3b2fba73c8da92820c31eb29da
jkc1996 <jaykumarchaudhary.nmims@gmail.com>
2020-04-16 13:37:35 +0530

    1 file modified

--- App.js
+++ App.js
@@ -1,7 +1,71 @@
 import React from 'react';
 class App extends React.Component {
   render() {
-    return React.createElement("h1", {}, "Hello World");
-  }
+
+    // jsx syntax writing
+
+    // return( <form> <h2>Login</h2>
+    //       <input type="text" placeholder="Name" /><br/><br/>
+    //       <input type="password" placeholder="password" /> <br/><br/>
+    //       <input type="submit" nvalue="log" />
+    //       </form>
+    //       )
+    // --------------------------------------------------------------
+    
+    // javascript expression in JSX
+
+    //  let highlight = {
+    //                    color: 'green',backgroundColor:'grey'
+    //             }
+    //            return(<React.Fragment>
+    //                   <h1 style={highlight}>Welcome to React</h1> 
+    //            </React.Fragment>)
+
+    //-------------------------------------------------------------
+
+    // conditional rendering of element using if-else
+
+  //    let isLoggedIn = true
+  //              if(isLoggedIn) {
+  //                      return <h2>Welcome Admin</h2>
+  //               }
+  //              else{
+  //                       return <h2>Please Login</h2>
+  //              }  
+
+  // }
+
+  // -------------------------------------------------------------
+
+  // conditional rendering of element using if-else 2
+
+  // let element = null;
+  // let isLoggedIn = false
+  // if(isLoggedIn) {
+  //         element = <h2>Welcome Admin</h2>
+  // }
+  // else {
+  //         element = <h2>Please Login</h2>
+  // }  
+  // return (<React.Fragment>
+  //         {element}
+  // </React.Fragment>)
+  // } 
+
+// -------------------------------------------------------------
+
+// conditional rendering of element using turnery
+
+// let element = null;
+//   let isLoggedIn = false;
+
+//   (isLoggedIn) ?  element = <h2>Welcome Admin</h2> : element = <h2>Please Login</h2>;
+          
+//   return (<React.Fragment>
+//           {element}
+//   </React.Fragment>)
+//   } 
+
+// --------------------------------------------------------------
 }
 export default App;
