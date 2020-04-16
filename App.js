6ad9f7291811e31fe6af7e07689d0484d2e22457
jkc1996 <jaykumarchaudhary.nmims@gmail.com>
2020-04-16 13:53:21 +0530

    1 file modified

--- App.js
+++ App.js
@@ -67,5 +67,37 @@ class App extends React.Component {
 //   } 
 
 // --------------------------------------------------------------
+
+// looping using map() method
+
+var employees = [
+    { empId: 1234, name: 'John', designation:'SE', salary:23000 },
+    { empId: 4567, name: 'Tom', designation:'SSE', salary:15000 },
+    { empId: 8910, name: 'Kevin',designation:'TA', salary:20000 }
+  ]
+
+  return (<React.Fragment>
+          <table style={{width:'60%'}} className='table'>
+              <thead className="thead-light">
+                  <tr>
+                        <th>EmpID</th>
+                        <th>Name</th>
+                        <th>Designation</th>
+                  </tr>
+              </thead>
+              <tbody>
+                    {employees.map(employee => {
+                          return employee.salary > 15000 ? (<tr  key={Math.random()}>
+                                  <td>{employee.empId}</td>
+                                  <td>{employee.name}</td>
+                                  <td>{employee.designation}</td>
+                          </tr>) : null
+                  })
+                  }
+              </tbody>
+          </table>
+  </React.Fragment>)
+  
+  }
 }
 export default App;
