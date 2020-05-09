aed9468f25be1d2fb2039a9591a98c6def01a893
jkc1996 <jaykumarchaudhary.nmims@gmail.com>
2020-05-09 13:15:45 +0530

    1 file modified

--- Timer.js
+++ Timer.js
@@ -20,27 +20,53 @@ class Timer extends React.Component {
 
 
 // bllow code explains the async nature of setState()
-        constructor() {
-                super()
-                this.state = {
-                        quantity: 0,
-                        price:0
-                }
-        }
-         update = () => {
-                this.setState({quantity:5},()=>{
-                        if(this.state.quantity == 5) {
-                                this.setState({price:2000-100})
-                        }
-                })
-        }
+        // constructor() {
+        //         super()
+        //         this.state = {
+        //                 quantity: 0,
+        //                 price:0
+        //         }
+        // }
+        //  update = () => {
+        //         this.setState({quantity:5},()=>{
+        //                 if(this.state.quantity == 5) {
+        //                         this.setState({price:2000-100})
+        //                 }
+        //         })
+        // }
 
+        // render() {
+        //        return(<React.Fragment>
+        //                 <h1>{this.state.quantity}</h1>
+        //                 <h1>{this.state.price}</h1>
+        //                 <button onClick={this.update}>Update</button>
+        //         </React.Fragment>)}
+
+
+      // another example of state
+      
+        constructor() {
+            super();
+            this.state = {
+                secondsElapsed: 0
+            };
+        }
+        start = () => {
+            this.setState({
+                secondsElapsed: this.state.secondsElapsed + 1
+            });
+        }
+        handleClick = (e) => {
+            this.interval = setInterval(this.start, 1000);
+        }
         render() {
-               return(<React.Fragment>
-                        <h1>{this.state.quantity}</h1>
-                        <h1>{this.state.price}</h1>
-                        <button onClick={this.update}>Update</button>
-                </React.Fragment>)}
+            return ( <React.Fragment><br/><br/>
+                <button onClick = {this.handleClick}>Start timer</button><br/><br/>
+                <h2> Seconds Elapsed: 
+                    {this.state.secondsElapsed} 
+                </h2> 
+                </React.Fragment>);
+            }
                
 }
 
