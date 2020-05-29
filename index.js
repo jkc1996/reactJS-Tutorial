b62360347083ce7c2edc10827313603808b6d3c1
jkc1996 <jaykumarchaudhary.nmims@gmail.com>
2020-05-29 18:17:36 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import App from './App';
 import Timer from './Timer';
+import Parent from './Parent';
+import Child from './Child';
 import './style.css';
 
 // class App extends Component {
